df66244c9948215abe3cc622d8e1feefa7179cdb
prototype2010 <alex.zakorko2015@gmail.com>
2024-04-01 12:19:11 +0300

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: '/business-card',
+    output: "export",
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
